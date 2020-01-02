2f4dbaae277a45e7d8a8ca059bb0d43e786e80a1
cyrussembrano <cyrus.sembrano@flowsoftware.co.nz>
2020-01-03 11:01:19 +1300

    packages

--- 1/app.py
+++ 1/app.py
@@ -6,20 +6,25 @@ a = 20
 guess_count = True
 
 # integer division
+print("integer division")
 print(10 // 3)
 # exponents
+print("exponents")
 print(10 ** 3)
 # augmented assignment operator
+print("augmented assignment operator")
 x = 10
 x += 3
 print(x)
 
 # operator precedence
 # P-E-M-D-A-S
+print("P-E-M-D-A-S")
 y = (10 + 3) * 2 ** 2
 print(y)
 
 # math functions
+print("math functions")
 w = 2.9
 print(round(w))
 print(abs(-2.9))
---
+++ 1/builtinmodules.py
@@ -0,0 +1,6 @@
+import random
+
+for i in range(3):
+    print(random.random())
+    print(random.randint(10,20))
+    
